afef9173a34c8f787991cd2dd905661dfca87734
LarsKarlsen82 <lpuk82@gmail.com>
2024-01-08 20:02:40 +0100

    update

--- src/components/WhiteArticle/WhiteArticle.jsx
+++ src/components/WhiteArticle/WhiteArticle.jsx
@@ -5,9 +5,23 @@ import styles from './WhiteArticle.module.scss';
 const WhiteArticle = () => {
   return (
     <div className={styles.WhiteArticle}>
-      <h2>Another Article Title</h2>
-      <p className={styles.paragraph}>
-        Another article content goes here. You can customize it as needed.
+      <h2>Under the Moon - New style</h2>
+      <p className={styles.whiteP}>
+      Lorem ipsum dolor sit amet. 
+      Et repudiandae vitae sed alias sequi non quia nobis quo voluptas voluptas eos consectetur accusamus. 
+      Eum nulla expedita ut internos corrupti qui debitis tempora.
+
+      Est animi magni nam vero voluptas cum esse aliquid qui soluta quia et explicabo voluptatem sit commodi dolorem. 
+      Ex deserunt deleniti ut consectetur officiis id iusto quaerat aut quia sunt ex obcaecati cumque ut dolorem tempora.
+      Est dolore internos quo soluta quas et quisquam facere.
+      At repudiandae impedit eos veritatis molestias qui quidem dolor. 
+      Ut veniam amet 33 eius commodi non tempora soluta aut quaerat.
+      Ullam sit ratione excepturi est facilis expedita qui perspiciatis unde. 
+      Vel libero explicabo qui velit iure non dolorem ipsam non eligendi natus 
+      qui quibusdam libero sit consectetur repellat?
+        <a href="#" className={styles.link}>
+          Læs mere
+        </a>
       </p>
     </div>
   );
